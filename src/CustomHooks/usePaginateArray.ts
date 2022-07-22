bccbc01b742408aa3b2508f1dec886aaead58ed2
CursedRock17 <mtglucas1@gmail.com>
2022-07-22 15:18:14 -0400

    Fixing Build Errors Vercel pt7

--- src/CustomHooks/usePaginateArray.ts
+++ src/CustomHooks/usePaginateArray.ts
@@ -1,9 +1,12 @@
 function usePaginateArray(pageNum:number, perPage:number, listToPaginate:Array<any>){
-    const indexOfLastDoc = pageNum * perPage;
-    const indexOfFirstDoc = indexOfLastDoc - perPage;
-    const currentUsers = listToPaginate.slice(indexOfFirstDoc, indexOfLastDoc);
-
-    return currentUsers
+    if(pageNum && perPage && listToPaginate){
+        const indexOfLastDoc = pageNum * perPage;
+        const indexOfFirstDoc = indexOfLastDoc - perPage;
+        const currentUsers = listToPaginate.slice(indexOfFirstDoc, indexOfLastDoc);
+    
+        return currentUsers
+    }
+    return []
 }
 
 export default usePaginateArray
